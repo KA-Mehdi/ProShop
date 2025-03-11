9a71a2724a2b538eccf52f9a644e3eb510f9faec
mehdi <kamilmehdi449@gmail.com>
2025-03-11 02:43:10 +0000

    fetch data from the products file with axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeScreen = () => {
-  return <>
-    <h1>Latest Products</h1>
-    <Row>
-        {products.map(pro=> {
-            return (
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await axios('http://localhost:5000/api/products')
+      setProducts(data)
+    };
+  
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((pro) => {
+          return (
             <Col key={pro._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={pro} />
+              <Product product={pro} />
             </Col>
-            )
+          );
         })}
-    </Row>
-  </>
-}
+      </Row>
+    </>
+  );
+};
 
 export default HomeScreen;
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,73 +1,96 @@
-import React from "react";
+import { React } from "react";
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
-import { Row, Col, Image,ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
+import { useEffect,useState } from "react";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
-
+import axios from "axios";
 
 const ProductScreen = () => {
-  const { id: productId } = useParams();
-  // find where the product.id equal the id in the params(link)
-  const product = products.find((p) => p._id === productId)
+  const [product, setProduct] = useState({});
 
-  return <>
-    <Link className="btn btn-light my-3" to='/'>
+  const { id: productId } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(
+        `http://localhost:5000/api/products/${productId}`
+      );
+      setProduct(data);
+    };
+    fetchProduct()
+  }, [productId]);
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
         Go Back
-    </Link>
-    <Row>
+      </Link>
+      <Row>
         <Col md={5}>
-            <Image src={product.image} alt={product.name} fluid/>
+          <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={4}>
-            <ListGroup variant="flush">
-                <ListGroup.Item>
-                    <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroupItem>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews` }/>
-                </ListGroupItem>
-                <ListGroupItem>
-                    Price: ${product.price}
-                </ListGroupItem>
-                <ListGroupItem>
-                    description: ${product.description}
-                </ListGroupItem>
-            </ListGroup>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>description: ${product.description}</ListGroupItem>
+          </ListGroup>
         </Col>
         <Col md={3}>
-            <Card>
-                <ListGroup variant="flush">
-                    <ListGroupItem>
-                        <Row>
-                            <Col>Price: </Col>
-                            <Col>
-                                <strong>${product.price}</strong>
-                            </Col>
-                        </Row>
-                    </ListGroupItem>
-                </ListGroup>
-                <ListGroup variant="flush">
-                    <ListGroupItem>
-                        <Row>
-                            <Col>Status: </Col>
-                            <Col>
-                                <strong>
-                                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                                </strong>
-                            </Col>
-                        </Row>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
-                            Add To Card
-                        </Button>
-                    </ListGroupItem>
-                </ListGroup>
-            </Card>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price: </Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            </ListGroup>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Status: </Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Card
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
         </Col>
-    </Row>
-  </>;
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
