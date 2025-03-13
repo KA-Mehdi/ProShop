657411ae27d759c091419f6e8e2b94e50b5ce3b8
mehdi <kamilmehdi449@gmail.com>
2025-03-13 03:13:02 +0000

    adding controllers to routes

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,22 +1,10 @@
 import express from "express";
 const  router = express.Router();
-import asyncHandler from "express-async-handler";
-import Product from '../modals/productModal.js'
+import {getProducts, getProductById} from '../controller/productContollers.js'
 
-router.get('/', async (req, res) => {
-    const products = await Product.find({})
-    res.json(products);
-});
+router.route('/'). get(getProducts)
 
-router.get('/:id', asyncHandler (async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}));
+router.route('/:id').get(getProductById)
 
 
 export default router
