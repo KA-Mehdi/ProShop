20dd20e9f5fa845153b1cd482cdcc806ecb40632
mehdi <kamilmehdi449@gmail.com>
2025-03-13 02:28:47 +0000

    gte products from database

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
+import productRoutes from './routes/productsRoutes.js'
+
 
 dotenv.config();
 
@@ -20,18 +22,8 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id.toString() === req.params.id); // Ensure proper comparison
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product not found' });
-    }
-});
 
 // Start the server
 const PORT = process.env.PORT || 8000;
