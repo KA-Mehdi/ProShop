95ed288cecea1df988d6e43faed60e10f776e320
mehdi <kamilmehdi449@gmail.com>
2025-03-20 01:57:11 +0000

    adding the users slices related to the authentification

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// check if there is a data in localstorage with the key 'userInfo'
+const initialState = {
+    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducer: {
+        setCredentials: (state, action) => {
+            state.userInfo = action.payload;
+            localStorage.setItem('userInfo', JSON.stringify(action.payload))
+        }
+    }
+})
+
+export const { setCredentials } = authSlice.actions; 
+export default authSlice.reducer
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import carteSliceReducer from "./slices/carteSlice"
+import authSliceReducer from "./slices/authSlice"
+
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart: carteSliceReducer
+    cart: carteSliceReducer,
+    auth: authSliceReducer 
+
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
